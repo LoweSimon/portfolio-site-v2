20ca5ee98d4861e7f07d6a5e6ddc82268b67f4cf
LoweSimon <simon.lowe90@hotmail.co.uk>
2022-11-04 11:36:45 +0000

    Working on design

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,14 @@
-// Import custom CSS
-import '../scss/styles.scss'
-
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-
 const btn = document.getElementById('button');
+const faders = document.querySelectorAll('.fade-in');
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
+
+
+document.getElementById('form').addEventListener('submit', function(event) {
    event.preventDefault();
 
    btn.value = 'Sending...';
 
-   const serviceID = 'default_service';
+   const serviceID = 'service_ipfon8q';
    const templateID = 'template_g3to4r7';
 
    emailjs.sendForm(serviceID, templateID, this)
@@ -24,3 +20,35 @@ document.getElementById('form')
       alert(JSON.stringify(err));
     });
 });
+
+
+// const appearOptions = {}
+
+// const appearOnScroll = new IntersectionObserver 
+// (function(
+//     entries,
+//     appearOnScroll
+//   ) {
+//     entries.forEach(entry =>  {
+//       if (!entry.isIntersecting)  {
+//         return;
+//       } else {
+//         entry.target.classList.add('appear');
+//         appearOnScroll.unobserve(entry.target);
+//       }
+//     })
+//   }, 
+//   appearOptions);
+
+//   faders.forEach(fader => {
+//     appearOnScroll.observe(fader);
+//   })
+
+const appearOnScroll = new IntersectionObserver
+(function
+  (
+    entries, 
+    appearOnScroll
+    ) 
+    {}, 
+    options)
